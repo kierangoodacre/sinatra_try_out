34a7695ec4b516233ff2a6c6cf98f1eff9d20e3a
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-06 11:33:21 +0000

    added cat to unique web page

--- hello.rb
+++ hello.rb
@@ -1,9 +1,7 @@
 require 'sinatra'
 
 get '/' do
-	'hello!, heyllo again'
-end
-
-get '/secret' do
-	'This is a secret page'
+	"<div style='border: 3px dashed red'>
+	<img src='http://bit.ly/1eze8aE'>
+	</div>"
 end
